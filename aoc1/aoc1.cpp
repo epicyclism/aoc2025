b6e1964b35ebbf501b09e1b071bce9835fb33b0d
Paul <paul@epicyclism.com>
2025-12-02 12:38:44 +0000

    aoc2

--- aoc1/aoc1.cpp
+++ aoc1/aoc1.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <vector>
 
 #include <fmt/format.h>
 
@@ -11,26 +9,61 @@
 
 auto get_input()
 {
-	return 0;
+	std::vector<int> v;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+	{
+		std::string_view s(ln);
+		s.remove_prefix(1);
+		auto tmp = sv_to_t<int>(s);
+		v.emplace_back(ln[0] == 'L' ? tmp * -1 : tmp);
+	}
+	return v;
 }
 
-int64_t pt1(auto const& in_addr_t)
+auto pt12(auto const& in)
 {
-	timer t("p1");
-	return 0;
-}
-
-int64_t pt2(auto const& in)
-{
-	timer t("p2");
-	return 0;
+	timer t("p12");
+	int p = 50;
+	int p1 = 0;
+	int p2 = 0;
+	for(auto t: in)
+	{
+		if(t < 0)
+		{
+			p2 -= t / 100;
+			t %= 100;
+			for(int n = t; n < 0; ++n)
+			{
+				--p;
+				p2 += p == 0;
+				if(p < 0)
+					p = 99;
+			}
+		}
+		else
+		{
+			p2 += t / 100;
+			t %= 100;
+			for(int n = 0; n < t; ++n)
+			{
+				++p;
+				if(p == 100)
+				{
+					++p2;
+					p = 0;
+				}
+			}
+		}
+		p1 += p == 0;
+	}
+	return std::make_pair(p1, p2);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
--- aoc2/aoc2.cpp
+++ aoc2/aoc2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <array>
 #include <string>
+#include <charconv>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -9,28 +12,74 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
+auto parse_range(std::string_view s)
+{
+	auto p = s.find('-');
+	auto f = sv_to_t<int64_t>(std::string_view(s.begin(), p));
+	auto t = sv_to_t<int64_t>(std::string_view(s.begin() + p + 1, s.size() - p));
+	return std::make_pair(f, t);
+}
+
 auto get_input()
 {
-	return 0;
+	std::vector<std::pair<int64_t, int64_t>> rv;
+	std::string ln;
+	std::getline(std::cin, ln);
+	for(auto const v : std::views::split(ln, ','))
+		rv.emplace_back(parse_range(std::string_view(v)));
+	return rv;
 }
 
-int64_t pt1(auto const& in_addr_t)
+bool invalid1(std::string_view sv)
 {
-	timer t("p1");
-	return 0;
+	if(sv.size() & 1)
+		return false;
+	return sv.substr(0, sv.size() / 2) == sv.substr(sv.size() / 2);
 }
 
-int64_t pt2(auto const& in)
+inline bool my_test(std::string_view sv, int n)
 {
-	timer t("p2");
-	return 0;
+	auto component = sv.substr(0, n);	
+	for(int o = n; o < sv.size(); o += n)
+		if(sv.substr(o, n) != component)
+			return false;
+	return true;
+}
+
+bool invalid2(std::string_view sv)
+{
+	for(int n = 1; n <= sv.size() / 2; ++n)
+		if(sv.length() % n == 0 && my_test(sv, n))
+			return true;
+	return false;
+}
+
+auto pt12(auto const& in)
+{
+	timer t("p12");
+	int64_t p1 = 0;
+	int64_t p2 = 0;
+	const size_t sz = 32;
+	char buf[sz];
+	for(auto[f, t] : in)
+	{
+		for(; f <= t; ++f)
+		{
+			auto cr = std::to_chars(buf, buf + sz, f);
+			std::string_view sv(buf, cr.ptr - buf);
+			if(invalid1(sv))
+				p1 += f;
+			if(invalid2(sv))
+				p2 += f;
+		}
+	}
+	return std::make_pair(p1, p2);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
