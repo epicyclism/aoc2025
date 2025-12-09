3fe85af4113b247116ffdab6872301bc5179c79e
Paul Ranson <paul@epicyclism.com>
2025-12-09 12:24:12 +0000

    aoc9 pt v. slow

--- aoc9/aoc9.cpp
+++ aoc9/aoc9.cpp
@@ -29,81 +29,137 @@ int64_t pt1(auto const& in)
 	timer t("p1");
 	auto area = [](auto a, auto b){ return (std::abs(a.first - b.first) + 1) * (std::abs(a.second - b.second) + 1);};
 	int64_t p1 = 0;
-	for(auto y = 0; y < in.size() - 1; ++y)
-		for(auto x = y + 1; x < in.size(); ++x)
-		{
-			auto ar = area(in[x], in[y]);
-//			fmt::println("{} {} - {} {} = {}", in[x].first, in[x].second, in[y].first, in[y].second, ar);
-			p1 = ar > p1 ? ar : p1;
-		}
+	for (auto y = 0; y < in.size() - 1; ++y)
+		for (auto x = y + 1; x < in.size(); ++x)
+			p1 = std::max(p1, area(in[x], in[y]));
+
 	return p1;
 }
-/*
-def point_in_polygon(point, polygon):
-x, y = point
-num_vertices = len(polygon)
-inside = False
 
-p1x, p1y = polygon[0]
-for i in range(num_vertices + 1):
-	p2x, p2y = polygon[i % num_vertices]
-	if y > min(p1y, p2y):
-		if y <= max(p1y, p2y):
-			if x <= max(p1x, p2x):
-				if p1y != p2y:
-					x_intersection = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-				if p1x == p2x or x <= x_intersection:
-					inside = not inside
-	p1x, p1y = p2x, p2y
-
-return inside
-*/
+// after a Stack Overflow point in polygon answer...
 bool inside(auto p, auto const& vp)
 {
+	auto between = [](auto t, auto a, auto b ) -> bool { return (t >= a && t <= b) || (t <= a && t >= b); };
 	bool in = false;
+
 	auto i1 = vp.begin();
 	auto i2 = i1 + 1;
-	while(i2 != vp.end())
+	for(; i2 != vp.end(); ++i1, ++i2)
 	{
-		if(p.second > std::min((*i1).second, (*i2).second))
-			if(p.second <= std::max((*i1).second, (*i2).second))
-				if(p.first <= std::max((*i1).first, (*i2).first))
-				{
-					double xi = -1;
-					if((*i1).second != (*i2).second)
-						xi = double(p.second - (*i1).second) * ((*i2).first - (*i1).first) / ((*i2).second - (*i1).second) + (*i1).first;
-					if((*i1).first == (*i2).first || p.first <= xi)
-						in = !in;
-				}
-		++i1;
-		++i2;
+		auto A = *i1;
+		auto B = *i2;
+		if (p == A || p == B)
+			return true;
+		if (A.second == B.second && p.second == A.second && between(p.first, A.first, B.first))
+			return true;
+		if (between(p.second, A.second, B.second))
+		{
+			if (p.second == A.second && B.second >= A.second || p.second == B.second && A.second >= B.second)
+				continue;
+			auto c = (A.first - p.first) * (B.second - p.second) - (B.first - p.first) * (A.second - p.second);
+			if (c == 0)
+				return true;
+			if ((A.second < B.second) == c > 0)
+				in = !in;
+		}
 	}
-
 	return in;
 }
 
 bool all_inside(auto a, auto b, auto const& vp)
 {
-	auto ul = a;
-	auto br = b;
-	if(a.first > b.first)
+	auto A = std::make_pair(a.first, b.second);
+	auto B = std::make_pair(b.first, a.second);
+	if (inside(A, vp) && inside(B, vp))
 	{
-		ul.first = b.first;
-		br.first = a.first;
+		// check the perimeter...
+		// A - a
+		if (a.second > A.second)
+		{
+			auto tt = a;
+			for (auto t = A.second; t <= a.second; ++t)
+			{
+				tt.second = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		else
+		{
+			auto tt = a;
+			for (auto t = a.second; t <= A.second; ++t)
+			{
+				tt.second = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		// B - b
+		if (b.second > B.second)
+		{
+			auto tt = b;
+			for (auto t = B.second; t <= b.second; ++t)
+			{
+				tt.second = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		else
+		{
+			auto tt = a;
+			for (auto t = b.second; t <= B.second; ++t)
+			{
+				tt.second = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		// A - b
+		if (A.first > b.first)
+		{
+			auto tt = b;
+			for (auto t = b.first; t <= A.first; ++t)
+			{
+				tt.first = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		else
+		{
+			auto tt = b;
+			for (auto t = A.first; t <= b.first; ++t)
+			{
+				tt.first = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		// B - a
+		if (a.first > B.first)
+		{
+			auto tt = a;
+			for (auto t = B.first; t <= a.first; ++t)
+			{
+				tt.first = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		else
+		{
+			auto tt = a;
+			for (auto t = a.first; t <= B.first; ++t)
+			{
+				tt.first = t;
+				if (!inside(tt, vp))
+					return false;
+			}
+		}
+		return true;
 	}
-	if(a.second > b.second)
-	{
-		ul.second = b.second;
-		br.second = a.second;
-	}
-	--br.first;
-	--br.second;
-	
-	auto ra = inside(std::make_pair(br.first, ul.second), vp);
-	auto rb = 
-			inside(std::make_pair(ul.first, br.second), vp);
-//	fmt::println("{},{} - {},{} {},{} - {},{}: {} {}", a.first, a.second, b.first, b.second, b.first, a.second, a.first, b.second, ra, rb);
-	return ra && rb;
+	return false;
 }
 
 int64_t pt2(auto const& in)
@@ -121,21 +177,12 @@ int64_t pt2(auto const& in)
 	return p2;
 }
 // 4620005060 too high
-//
 
-void test(auto const& in)
-{
-	fmt::println("2, 3 {}", inside(std::make_pair(2, 3), in));
-	fmt::println("9, 5 {}", inside(std::make_pair(9, 5), in));
-	fmt::println("9, 3 {}", inside(std::make_pair(9, 3), in));
-	fmt::println("2, 5 {}", inside(std::make_pair(2, 5), in));
-}
 int main()
 {
 	auto in = get_input();
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
-	test(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
